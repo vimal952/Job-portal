2ff4ace8947a584c54368bc04dc79e9d81f2e116
Vimal <tvimal002@gmail.com>
2026-03-04 02:01:06 +0530

    ok

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@
 
  const connectDB=async()=>{
     mongoose.connection.on('connected',()=>console.log('MongoDB connected successfully'));
-    await mongoose.connect(`${process.env.MONGODB_URI}/job_portal`)
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
  }
  export default connectDB; 
